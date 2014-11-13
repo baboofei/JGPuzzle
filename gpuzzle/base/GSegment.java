d0bd916366ec7895f4792a39f1ba84e329c5248e
Claude Luo <babooclaude@hotmail.com>
2014-11-13 14:06:41 -0500

    Implemented GSegment.GSentence, however lacking the modPolicy feature

--- gpuzzle/base/GSegment.java
+++ gpuzzle/base/GSegment.java
@@ -14,11 +14,13 @@ public class GSegment implements GCollection
 		}
 	}
 
+	// Return a ListIterator object containing all GeneBases in this.Contents
 	public ListIterator<GeneBase> GetContents()
 	{
 		return this.Contents.listIterator();
 	}
 
+	// Return a ListIterator object containing all GeneBases in this.Contents with given size and endian
 	public ListIterator<GeneBase> GetContents(int sizeBytes, GEndian endian)
 	{
 		if(endian == GEndian.Low)
@@ -31,9 +33,26 @@ public class GSegment implements GCollection
 		}
 	}
 
-	public GSentence ToGSentence()
+	// Creates a GSentence object and convert all current GeneBases in this.Contents to GBytes
+	public GSentence ToGSentence(int byteSize, int modPolicy)
 	{
-		// TODO : Implement this as slicing the GeneBases into GBytes
-		throw new UnsupportedOperationException("Not implemented");	
+		// byteSize is the size of byte in the GSentence object. All GBytes will have size byteSize
+		// modPolicy determines how will the method treat the 'extra' GeneBases at the beginning / ending of the GSegment
+		// 0 for default (remove at the end), 1 for remove at the end, 2 for remove at the front, 3 for return empty GSentence if any byte are to be left over
+
+		// TODO : Actually implement the modPolicy feature
+
+		ArrayList<GByte> resultBytes = new ArrayList<GByte>();
+
+		for(int currentBasePosition = 0; currentBasePosition < this.Contents.size() && currentBasePosition + byteSize <= this.Contents.size(); currentBasePosition += byteSize)
+		{
+			GeneBase[] currentByteBases = new GeneBase[byteSize];			
+			
+			this.Contents.subList(currentBasePosition, currentBasePosition + byteSize).toArray(currentByteBases);
+
+			resultBytes.add(new GByte(currentByteBases));
+		}
+
+		return new GSentence(resultBytes.listIterator());
 	}
 }
